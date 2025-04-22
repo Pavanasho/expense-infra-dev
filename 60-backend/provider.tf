7668f8bb93210e5daf06ae3de8c610dbd8d1b4c9
Ashok <pavanashok5584@gmail.com>
2025-04-22 23:13:57 +0530

    expense infra dev added

--- 60-backend/provider.tf
+++ 60-backend/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.93.0"
+      version = "5.84.0"
     }
   }
 
@@ -18,3 +18,24 @@ provider "aws" {
   # Configuration options
   region = "us-east-1"
 }
+
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "5.93.0"
+#     }
+#   }
+
+#   backend "s3" {
+#     bucket = "82s-tf-remote-state-dev-dpak"
+#     key    = "expense-dev-backend" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
+#     region = "us-east-1"
+#     dynamodb_table = "82s-tf-remote-state-lock-dev-dpak"
+#   }
+# }
+
+# provider "aws" {
+#   # Configuration options
+#   region = "us-east-1"
+# }
