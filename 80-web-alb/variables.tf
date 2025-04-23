21c074c8d659aac9ac52d2496aeb8f35353e5396
Ashok <pavanashok5584@gmail.com>
2025-04-23 23:09:35 +0530

    expense infra dev frontend added

---
+++ 80-web-alb/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+
+variable "zone_id" {
+    default = "Z099819629ZVWKL9S8414"
+}
+
+variable "domain_name" {
+    default = "dpak.online"
+}
